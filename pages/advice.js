6b09c0a325f1403dbdcb82418856846807d838d1
Katherine Smith <katherine.lo.smith@gmail.com>
2019-02-05 17:45:41 -0800

    Display giphy based on advice

--- pages/advice.js
+++ pages/advice.js
@@ -1,15 +1,25 @@
 import fetch from 'isomorphic-unfetch'
+import getConfig from 'next/config'
+const {publicRuntimeConfig} = getConfig()
 
 const Advice = (props) => (
-  <h1>{props.advice}</h1>
+  <div>
+    <h2>{props.advice}</h2>
+    <img src={props.giphy} alt="Giphy"></img>
+  </div>
 )
 
 Advice.getInitialProps = async function() {
-  const res = await fetch('https://api.adviceslip.com/advice')
-  const data = await res.json()
+  const adviceRes = await fetch('https://api.adviceslip.com/advice')
+  const adviceData = await adviceRes.json()
+  const advice = adviceData.slip.advice
+
+  const giphyRes = await fetch(`http://api.giphy.com/v1/gifs/translate?s=${advice}&weirdness=5&api_key=${publicRuntimeConfig.API_KEY}`)
+  const giphyData = await giphyRes.json()
 
   return {
-    advice: data.slip.advice
+    advice: advice,
+    giphy: giphyData.data.images.original.url
   }
 }
 
